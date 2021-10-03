98173a8527614e254f74ad4946839f9b895db748
Dahye Lee <melllamodahye@gmail.com>
2021-10-04 02:53:59 +0900

    :file_folder: 소스코드 java 하위 패키지 이동(java -> server)

---
+++ lib/src/test/java/server/TestChatServer.java
@@ -0,0 +1,44 @@
+package server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import server.ChatServer;
+
+import static org.mockito.Mockito.when;
+
+public class TestChatServer {
+
+    @Mock
+    private ChatServer server;
+
+    @Test
+    public void openServerSocket() throws IOException {
+        MockitoAnnotations.openMocks(this);
+        when(server.openServerSocket()).thenReturn(new ServerSocket());
+        ServerSocket serverSocket = server.openServerSocket();
+        Assert.assertNotNull(serverSocket);
+    }
+
+    @Test
+    public void crateSocketAddress() {
+        MockitoAnnotations.openMocks(this);
+        InetSocketAddress address;
+        when(server.createSocketAddress(8000)).thenReturn(new InetSocketAddress(8080));
+    }
+
+
+
+//    public void setUpServer() {
+//        MockitoAnnotations.openMocks(this);
+//        when(server.setUpServer()).thenReturn(true);
+//        when(server.setUpServer()).thenReturn(false);
+//        when(server.setUpServer()).thenReturn(false);
+//    }
+
+}
